2dd5dc9853d1cdcf51b1628f1894435b69cd6428
Meyer <jmeyer7@pattersoncompanies.com>
2018-02-21 16:07:47 -0600

    initial commit for my angular training application.

---
+++ Angular4DotNetMvc/Controllers/CoursesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Angular4DotNetMvc.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            return View("Index", "", "you");
+        }
+    }
+}
