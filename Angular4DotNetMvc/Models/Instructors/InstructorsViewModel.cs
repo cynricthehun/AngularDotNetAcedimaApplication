7ef4db0504e3d5e9ec9676237bfae2bb8ffce260
Meyer <jmeyer7@pattersoncompanies.com>
2018-02-22 08:43:08 -0600

    added instructors view model

---
+++ Angular4DotNetMvc/Models/Instructors/InstructorsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Angular4DotNetMvc.Models.Instructors
+{
+    public class Instructors
+    {
+    }
+}
