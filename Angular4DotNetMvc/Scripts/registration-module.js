86d4b8d464ea1ab0426774f601f45a2480824b23
Meyer <jmeyer7@pattersoncompanies.com>
2018-02-23 08:24:45 -0600

    fixed issue where courses/instructors wasn't rendering.

--- Angular4DotNetMvc/Scripts/registration-module.js
+++ Angular4DotNetMvc/Scripts/registration-module.js
@@ -1,5 +1,5 @@
 ﻿var registrationModule = angular.module("registrationModule", []).config(function($routeProvider, $locationProvider){
-    $routeProvider.when('/Registration/Courses', {templateUrl: '/templates/courses.html', controller: 'CoursesController' });
-    $routeProvider.when('/Registration/Instructors', { templateUrl: '/templates/instructors.html', controller: 'InstructorsController' });
-    $locationProvider.html5Mode(True);
+    $routeProvider.when('/Registration/Courses', { templateUrl: '/Angular4DotNetMvc/templates/courses.html', controller: 'CoursesController' });
+    $routeProvider.when('/Registration/Instructors', { templateUrl: '/Angular4DotNetMvc/templates/instructors.html', controller: 'InstructorsController' });
+    $locationProvider.html5Mode(true);
 });
